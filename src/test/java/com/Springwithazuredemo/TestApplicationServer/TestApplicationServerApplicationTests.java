9fbe63fb983686f79327f8fcc277862f8e766599
SKundu2003 <kundu9831562286@gmail.com>
2023-05-08 01:20:44 +0530

    service created

---
+++ src/test/java/com/Springwithazuredemo/TestApplicationServer/TestApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springwithazuredemo.TestApplicationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
